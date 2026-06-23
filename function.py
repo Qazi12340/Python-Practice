a833d34e9a124db31eeab1e4c2ad3b7a331e93b8
Qazi12340 <qazituheed9@gmail.com>
2026-06-23 16:01:03 +0500

    First project - functions practice

---
+++ function.py
@@ -0,0 +1,23 @@
+
+def square (n):
+    return n * n
+print(square (4))
+
+def is_even(n):
+    return n%2==0
+print(is_even (7))
+
+def full_name (first , last):
+    return first + "  " + last
+print(full_name("Ali" , "khan"))
+
+def max_num (a , b):
+    if a > b :
+        return a
+    return b
+print(max_num(10 , 20))
+
+def celcius_to_fahrenheit(c):
+    return (c * 9/5) + 32
+print(celcius_to_fahrenheit(100))
+
